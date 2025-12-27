09fc94256687b0089dda7446583061fc34abb7af
Abdulsemiu Atanda <semiu@live.com>
2025-12-27 17:19:25 -0500

    add encryptable macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,10 @@
 extern crate proc_macro;
+
+mod crypt;
+
+use crypt::encryptable_derive_macro2;
+
+#[proc_macro_derive(Encryptable, attributes(encryptable))]
+pub fn encryptable_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+  encryptable_derive_macro2(input.into()).unwrap().into()
+}
